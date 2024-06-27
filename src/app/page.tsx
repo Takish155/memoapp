cae20d05f2c1710c61858de8e1595685727924cc
Takish Lim <rimutakeshi155@gmail.com>
2024-06-27 21:17:09 +0900

    removed unccessarry code in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
 export default async function Home() {
-  const res = await fetch(
-    "https://challenge-server.tracks.run/memoapp/category",
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "X-ACCESS-TOKEN": "e7b1b7b0-6b7b-4b7b-8b7b-0b7b7b7b7b7b",
-      },
-      method: "GET",
-    }
-  );
-  if (!res.ok) {
-    return <main>error</main>;
-  }
-  const data = await res.json();
-  console.log(data);
-
-  return <main>a</main>;
+  return <main></main>;
 }
